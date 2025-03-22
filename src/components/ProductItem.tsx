d8647f4ba699d636454f8200f94dce82f6696c76
Ali Shariati <seyed.ali.shariati@gmail.com>
2025-03-22 18:32:55 +0330

    add product items component

---
+++ src/components/ProductItem.tsx
@@ -0,0 +1,30 @@
+export interface IProductItemProps {
+    id: string,
+    image: string,
+    title: string,
+    description: string,
+    price: number,
+}
+async function ProductItem({image, title , price}: IProductItemProps) {
+
+
+
+    return (
+        <div className='shadow-md'>
+
+            <img
+                src={image}
+            className='w-full h-64'
+                alt='product'
+            />
+            <div className='p-4  text-right'>
+                <h3 className='font-bold'>{title}</h3>
+                <p>
+                    قیمت : <span>{price}</span> تومان
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default ProductItem
