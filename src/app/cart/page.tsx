27ec22c2ace41abe2f63940999ee4b5f3ac22381
Ali Shariati <seyed.ali.shariati@gmail.com>
2025-03-22 18:32:24 +0330

    add cart page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,36 @@
+import CartItem from "@/components/CartItem";
+import Container from "@/components/Container";
+
+function Cart (){
+    return (
+        <Container>
+            <h1 className='my-4 p-4'>سبد خرید</h1>
+            <div className='grid grid-cols-12 gap-4'>
+                <div className='col-span-9'>
+                    <CartItem/>
+                    <CartItem/>
+                    <CartItem/>
+                </div>
+                <div className=' col-span-3 border border-gray-200 shadow-lg shadow-gray-100 p-4 rounded-md'>
+                    <p className='m-2'><span> قیمت کل : </span><span> ۱۰۰۰ </span><span>تومان</span></p>
+                    <p className='m-2'><span> سود شما از این خرید : </span><span> ۱۰۰۰ </span><span>تومان</span></p>
+                    <p className='m-2'><span> قیمت نهایی : </span><span> ۱۰۰۰ </span><span>تومان</span></p>
+                    <div>
+                        <input
+                            type='text'
+                            placeholder='کد تخفیف را وارد کنید'
+                               className='border border-gray-200 w-full h-12 rounded-md p-2'
+                        />
+                        <button className='px-4 py-2 rounded bg-gray-300 mt-4'>اعمال تخفیف</button>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </Container>
+
+    );
+}
+
+export default Cart
