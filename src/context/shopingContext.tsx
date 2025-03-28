602d0d5b61e2da99f6dc19ad8d008627bcf51360
Ali Shariati <seyed.ali.shariati@gmail.com>
2025-03-28 23:15:52 +0330

    add - handel product increment qty

--- src/context/shopingContext.tsx
+++ src/context/shopingContext.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {createContext, useState} from "react";
+import React, {createContext, useContext, useState} from "react";
 
 type ShoppingContextProviderProps ={
     children : React.ReactNode
@@ -10,15 +10,42 @@ type CartItems={
     qty: number
 }
 
-const ShoppingContext = createContext({})
+type TShoppingCartContext ={
+    cartItems: CartItems[],
+    handelIncreaseProductQty: (id: number) => void
+}
 
+const ShoppingContext = createContext({} as TShoppingCartContext)
+
+export const useShoppingCartContext= () => {
+    return useContext(ShoppingContext)
+}
 
 
 function ShoppingContextProvider({children}: ShoppingContextProviderProps) {
 
     const [cartItems , setCartItems] = useState<CartItems[]>([])
+
+    const handelIncreaseProductQty = (id: number) => {
+        setCartItems(currentItem =>{
+            let isNotProductExist = currentItem.find(item => item.id == id) == null
+            if(isNotProductExist){
+                return [...currentItem , {id , qty: 1}]
+            }
+            else {
+                return currentItem.map(item => {
+                    if (item.id == id) {
+                        return {...item , qty: item.qty + 1}
+                    } else {
+                        return item
+                    }
+                })
+            }
+        })
+    }
+
     return (
-        <ShoppingContext.Provider value={{cartItems}}>
+        <ShoppingContext.Provider value={{cartItems, handelIncreaseProductQty}}>
             {children}
         </ShoppingContext.Provider>
     );
