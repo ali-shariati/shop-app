ddb4214d92c3343d396e048a4f748eae4c865ea2
Ali Shariati <seyed.ali.shariati@gmail.com>
2025-03-29 22:26:49 +0330

    add - total qty and decrease qty with persian number

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -7,14 +7,14 @@ interface IAddToCartProps {
 }
 function AddToCart({id}: IAddToCartProps) {
 
-    const{cartItems, handelIncreaseProductQty, getProductQty} = useShoppingCartContext()
+    const{cartItems, handelIncreaseProductQty,handelDecreaseProductQty, getProductQty} = useShoppingCartContext()
 
     console.log(cartItems)
     return (
         <div className='mt-4'>
             <button onClick={()=> handelIncreaseProductQty(parseInt(id))} className='px-4 py-2 rounded bg-gray-200'>+</button>
-            <span className='m-2'> {getProductQty(parseInt(id))} </span>
-            <button className='px-4 py-2 rounded bg-gray-200'>-</button>
+            <span className='m-2'> {getProductQty(parseInt(id)).toString().replace(/\d/g, (d) => '۰۱۲۳۴۵۶۷۸۹'[+d])} </span>
+            <button onClick={()=> handelDecreaseProductQty(parseInt(id))} className='px-4 py-2 rounded bg-gray-200'>-</button>
         </div>
     );
 }
