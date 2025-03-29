f05481a5f59498f54b7230a95c570987eb6d3234
Ali Shariati <seyed.ali.shariati@gmail.com>
2025-03-29 22:25:44 +0330

    add - total qty

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,14 @@
 import Link from "next/link";
 import {usePathname} from "next/navigation";
 import Container from "@/components/Container";
+import {useShoppingCartContext} from "@/context/shopingContext";
 
 export default function Navbar() {
 
     const pathName = usePathname()
+
+    const {getTotalQty} = useShoppingCartContext()
+
     const navLinks = [
         {
             name: 'خانه ',
@@ -27,6 +31,10 @@ export default function Navbar() {
                         )}
                     </div>
                     <div>
+                        <span className='px-2  bg-red-500 text-white rounded-full ml-2 font-iran'>
+                           {getTotalQty.toString().replace(/\d/g, (d) => '۰۱۲۳۴۵۶۷۸۹'[+d])}
+
+                        </span>
                         <Link href={'/cart'}>سبد خرید</Link>
                     </div>
 
