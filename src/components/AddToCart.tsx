78feb6870b269ebea795d4c62abf8129058a5a81
Ali Shariati <seyed.ali.shariati@gmail.com>
2025-03-29 17:21:23 +0330

    add - pass the get product qty to product page

--- src/components/AddToCart.tsx
+++ src/components/AddToCart.tsx
@@ -7,13 +7,13 @@ interface IAddToCartProps {
 }
 function AddToCart({id}: IAddToCartProps) {
 
-    const{cartItems, handelIncreaseProductQty} = useShoppingCartContext()
+    const{cartItems, handelIncreaseProductQty, getProductQty} = useShoppingCartContext()
 
     console.log(cartItems)
     return (
         <div className='mt-4'>
             <button onClick={()=> handelIncreaseProductQty(parseInt(id))} className='px-4 py-2 rounded bg-gray-200'>+</button>
-            <span className='m-2'> 3 </span>
+            <span className='m-2'> {getProductQty(parseInt(id))} </span>
             <button className='px-4 py-2 rounded bg-gray-200'>-</button>
         </div>
     );
