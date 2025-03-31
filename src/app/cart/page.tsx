64d8a0679387c89f7a3595ed93ea9779f2a32728
Ali Shariati <seyed.ali.shariati@gmail.com>
2025-03-31 23:35:43 +0330

    add data of cart item to cart Item component

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,15 +1,21 @@
+"use client"
 import CartItem from "@/components/CartItem";
 import Container from "@/components/Container";
+import {useShoppingCartContext} from "@/context/shopingContext";
 
 function Cart (){
+    const {cartItems} = useShoppingCartContext()
+
     return (
         <Container>
             <h1 className='my-4 p-4'>سبد خرید</h1>
             <div className='grid grid-cols-12 gap-4'>
                 <div className='col-span-9'>
-                    <CartItem/>
-                    <CartItem/>
-                    <CartItem/>
+                    {cartItems.map((item) => (
+                        <div key={item.id}>
+                            <CartItem {...item} />
+                        </div>
+                    ))}
                 </div>
                 <div className=' col-span-3 border border-gray-200 shadow-lg shadow-gray-100 p-4 rounded-md'>
                     <p className='m-2'><span> قیمت کل : </span><span> ۱۰۰۰ </span><span>تومان</span></p>
