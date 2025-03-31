de826231a50cdda0b87af6bb7aec4f4fb418bed2
Ali Shariati <seyed.ali.shariati@gmail.com>
2025-03-31 23:36:36 +0330

    call api of cart item  with product id

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,23 +1,43 @@
-function CartItem(){
+import {useEffect, useState} from "react";
+import axios from "axios";
+import AddToCart from "@/components/AddToCart";
+
+interface ICartItemProps{
+    id : number,
+    qty : number,
+}
+interface ProductData{
+    id: string,
+    image: string,
+    title: string,
+    description: string,
+    price: number,
+}
+
+function CartItem({id, qty}: ICartItemProps) {
+const [productData , setProductData] = useState<ProductData | null>(null)
+    useEffect(() => {
+        axios.get(`http://localhost:7000/product/${id}`).then(res => {
+            const {data} = res
+            setProductData(data)
+            console.log(res.data)
+        })
+    }, []);
     return (
         <div className='bg-slate-50 mb-4 rounded-md'>
             <div className='grid grid-cols-12 '>
-                <div className='col-span-2'>
+                <div className='col-span-2 '>
                     <img
                         className='h-[150px] w-[150px] '
-                        src="https://www.nickfordphotography.co.uk/wp-content/uploads/2022/06/NFP_FS15-1024x683.jpg.webp"
+                        src={productData?.image}
                         alt="محصول"
                     />
                 </div>
-                <div className='col-span-10 text-right'>
-                    <h3 className='m-1'>نام محصول : </h3>
-                    <p className='m-1'><span>تعداد : </span><span>۳</span></p>
-                    <p className='m-1'><span> قیمت محصول : </span><span> ۱۰۰۰ </span><span>تومان</span></p>
-                    <div className='mt-4'>
-                        <button className='px-4 py-2 rounded bg-gray-200'>+</button>
-                        <span className='m-2'> ۳</span>
-                        <button className='px-4 py-2 rounded bg-gray-200'>-</button>
-                    </div>
+                <div className='col-span-10 text-right mt-2'>
+                    <h3 className='m-1'>نام محصول : {productData?.title}</h3>
+                    <p className='m-1'><span>تعداد : </span><span>{qty.toString().replace(/\d/g, (d) => '۰۱۲۳۴۵۶۷۸۹'[+d])}</span></p>
+                    <p className='m-1'><span> قیمت محصول : </span><span> {productData?.price.toString().replace(/\d/g, (d) => '۰۱۲۳۴۵۶۷۸۹'[+d])} </span><span>تومان</span></p>
+                    <AddToCart id={id.toString()}/>
                 </div>
             </div>
         </div>
