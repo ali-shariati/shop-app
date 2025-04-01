604e18d23783c6984c0fbbf102331e48a8fbaa5e
Ali Shariati <seyed.ali.shariati@gmail.com>
2025-04-02 02:58:42 +0330

    add pagination and page params

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,28 +1,38 @@
 import Container from "@/components/Container";
-// import Image from "next/image";
 import ProductItem from "@/components/ProductItem";
 import Link from "next/link";
-import {IProductItemProps} from "@/types/products";
+import { IProductList } from "@/types/products";
+import Pagination from "@/components/pagination";
 
-async function Store() {
+interface IStoreProps {
+    params: Promise<{}>,
+    searchParams: Promise<{page: string, per_page: string}>
+}
 
-    const result = await fetch("http://localhost:7000/products?_page=1&_per_page=5")
-    const data = (await result.json()) as IProductItemProps[]
+async function Store({searchParams}: IStoreProps) {
+
+    const page = (await searchParams).page ?? "1"
+    const perPage = (await searchParams).per_page ?? "8"
+
+    const result = await fetch(`http://localhost:7000/products?_page=${page}&_per_page=${perPage}`);
+    const response = await result.json() as IProductList;
+    // Assuming the response shape is { data: [...] }
+    const productData = response.data;
+    const totalPage = response.pages
 
     return (
         <Container>
             <div className='text-right py-4'>فروشگاه</div>
             <div className='grid grid-cols-4 gap-4 rtl'>
-                {
-                    data.map((item) =>(
-                        <Link key={item.id} href={`/store/${item.id}`}>
-                        <ProductItem {...item}/>
-                        </Link>
-                    ))
-                }
-
+                {productData?.map((productItem) => (
+                    <Link key={productItem.id} href={`/store/${productItem.id}`}>
+                        <ProductItem {...productItem} />
+                    </Link>
+                ))}
             </div>
+            <Pagination pageCount={totalPage} />
         </Container>
     );
 }
-export default Store
+
+export default Store;
